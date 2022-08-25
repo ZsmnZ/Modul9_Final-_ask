2949544f7bedfbb8e2f3c986db5df4b691a4d51a
SMN <SMN@SMN-PC>
2022-08-25 20:30:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Task1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ExceptionPractic
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            List<Exception> excptions = new List<Exception>
+            {
+                new FormatException("Недопустимый формат"),
+                new FileNotFoundException("Файл не существует"),
+                new TimeoutException("Время истекло"),
+                new DirectoryNotFoundException("Неверный путь к каталогу"),
+                new MyException("Недопустимый возраст")
+            };
+            foreach (Exception e in excptions)
+            {
+                try
+                {
+                    throw e;
+                }
+                catch (MyException exc)
+                {
+                    Console.WriteLine($"{exc.Message}");
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine($"{exc.Message}");
+                }
+            }
+        }
+        public class MyException : Exception
+        {
+            public MyException(string message) : base(message) { }
+        }
+    }
+}
---
+++ Task2/Task2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace Task
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            NumberReader numberReader = new NumberReader();
+            numberReader.EnterNumberEvent += ShowNumber;
+            while (true)
+            {
+                try
+                {
+                    numberReader.Read();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введено некоректное значение");
+                }
+            }
+        }
+        static void ShowNumber(int number)
+        {
+            List<string> names = new List<string>
+            {
+                "Аганесян",
+                "Бобков",
+                "Лунёв",
+                "Яковлев",
+                "Зотов"
+            };
+            switch (number)
+            {
+                case 1:
+                    names.Sort();
+                    names.ForEach(Console.WriteLine);
+                    break;
+                case 2:
+                    names.Sort();
+                    names.Reverse();
+                    names.ForEach(Console.WriteLine);
+                    break;
+            }
+        }
+    }
+    public class NumberReader
+    {
+        public delegate void EnterNumberDelegate(int number);
+        public event EnterNumberDelegate EnterNumberEvent;
+        public void Read()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Введите число 1 или 2");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number != 1 && number != 2) throw new FormatException();
+            NumberEntered(number);
+        }
+        protected virtual void NumberEntered(int number)
+        {
+            EnterNumberEvent?.Invoke(number);
+        }
+    }
+}
+
